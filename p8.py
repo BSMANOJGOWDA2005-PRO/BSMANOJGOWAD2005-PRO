dabb6b68a405f6e83a313371bb736ddb6e57a16b
Manoj Gowda <manojgowdamanu005@gmail.com>
2025-09-02 23:21:26 +0530

    Add files via upload

---
+++ p8.py
@@ -0,0 +1,12 @@
+print("enter the values of a and b")
+print("the values ae...")
+a=int(input("a= "))
+b=int(input("b= "))
+if a>b:
+    print("a is greater than b")
+elif a==b:
+    print("a is equal to b")
+elif a<b:
+    print("b is greater than a")
+else:
+    print("invalid")
